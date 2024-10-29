f3d98081ecd0d7f8477c8b867358aea399aad7b8
Dev Code <hellodev_code@outlook.com>
2024-10-30 00:12:30 +0530

    v 1.0

--- dist/assets/contentScript.js
+++ dist/assets/contentScript.js
@@ -1,5 +1,5 @@
 (function(){
-var c=(p,n,t)=>new Promise((s,a)=>{var i=e=>{try{r(t.next(e))}catch(o){a(o)}},l=e=>{try{r(t.throw(e))}catch(o){a(o)}},r=e=>e.done?s(e.value):Promise.resolve(e.value).then(i,l);r((t=t.apply(p,n)).next())});(function(){"use strict";document.body.insertAdjacentHTML("beforeend",`
+var c=(p,n,t)=>new Promise((s,r)=>{var i=e=>{try{a(t.next(e))}catch(o){r(o)}},l=e=>{try{a(t.throw(e))}catch(o){r(o)}},a=e=>e.done?s(e.value):Promise.resolve(e.value).then(i,l);a((t=t.apply(p,n)).next())});(function(){"use strict";document.body.insertAdjacentHTML("beforeend",`
 <style>
   /* Loader Styles */
   .loader {
@@ -35,7 +35,7 @@ var c=(p,n,t)=>new Promise((s,a)=>{var i=e=>{try{r(t.next(e))}catch(o){a(o)}},l=
     <textarea id="takeaways" name="takeaways" rows="10" style="flex-grow:1; padding: 10px; border: 1px solid #ccc; border-radius: 5px; margin-bottom: 15px; resize: none;" placeholder="How did you solve the problem ? Explain your thought process."></textarea>
     
     <div style="display: flex; justify-content: flex-end; gap: 10px; margin-top: 20px;">
-      <button id="cancelModalButton" style="background-color: #ccc; padding: 6px 20px; border: none; border-radius: 5px; cursor: pointer; font-weight: bold;">Cancel</button>
+      <button id="cancelModalButton" style="background-color: #ccc; padding: 6px 20px; border: none; border-radius: 5px; cursor: pointer; font-weight: bold;">Close</button>
       <button type="submit" id="submitModalButton" style="background:transparent; color: rgb(38 38 38 / 1); padding: 6px 20px; border: 1px solid; border-color: rgb(38 38 38 / 1); border-radius: 5px; cursor: pointer; font-weight: bold; position: relative;">
         <span id="submitButtonText">Submit</span>
         <div id="loadingSpinnerWrapper" class="spinnerWrapper" style="display: none;width: 100%;">
@@ -45,6 +45,6 @@ var c=(p,n,t)=>new Promise((s,a)=>{var i=e=>{try{r(t.next(e))}catch(o){a(o)}},l=
     </div>
   </div>
 </div>
-`),chrome.runtime.onMessage.addListener((n,t,s)=>{n.action==="openModal"&&(document.getElementById("extensionModalBackground").style.display="block",document.getElementById("modalMessage").textContent="")}),document.getElementById("cancelModalButton").addEventListener("click",()=>{document.getElementById("extensionModalBackground").style.display="none"}),document.getElementById("submitModalButton").addEventListener("click",n=>c(this,null,function*(){n.preventDefault();const t=document.getElementById("submitButtonText"),s=document.getElementById("loadingSpinner"),a=document.getElementById("loadingSpinnerWrapper");t.style.display="none",a.style.display="inline-block",s.style.display="inline-block";const i=document.getElementById("status").value,l=document.getElementById("remarks").value,r=document.getElementById("takeaways").value,e={remarks:l,takeaways:r,status:i};chrome.runtime.sendMessage({action:"submitProblem",data:e},o=>{t.style.display="inline",a.style.display="none",s.style.display="none";const d=document.getElementById("modalMessage");o.status==="success"?(d.textContent="Submission successful!",d.style.color="green"):(d.textContent=`Error: ${o.error}`,d.style.color="red")})}))})();
+`),chrome.runtime.onMessage.addListener((n,t,s)=>{n.action==="openModal"&&(document.getElementById("extensionModalBackground").style.display="block",document.getElementById("modalMessage").textContent="")}),document.getElementById("cancelModalButton").addEventListener("click",()=>{document.getElementById("extensionModalBackground").style.display="none"}),document.getElementById("submitModalButton").addEventListener("click",n=>c(this,null,function*(){n.preventDefault();const t=document.getElementById("submitButtonText"),s=document.getElementById("loadingSpinner"),r=document.getElementById("loadingSpinnerWrapper");t.style.display="none",r.style.display="inline-block",s.style.display="inline-block";const i=document.getElementById("status").value,l=document.getElementById("remarks").value,a=document.getElementById("takeaways").value,e={remarks:l,takeaways:a,status:i};chrome.runtime.sendMessage({action:"submitProblem",data:e},o=>{t.style.display="inline",r.style.display="none",s.style.display="none";const d=document.getElementById("modalMessage");o.status==="success"?(d.textContent="Submission successful!",d.style.color="green"):(d.textContent=`Error: ${o.error}`,d.style.color="red")})}))})();
 
 })();
